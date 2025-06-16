0e68b5f97f12de7cbec297b75215964334cdad91
joeloughran <joseph.a.loughran@gmail.com>
2025-06-16 15:33:51 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// TOGGLE MOBILE MENU
+
+document.addEventListener('DOMContentLoaded', function () {
+    const toggleButton = document.querySelector ('.navbar .mobile-menu-toggle');
+    const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+
+    toggleButton.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
+    });
+});
+
+// CHANGE NAVBAR BACKGROUND ON SCROLL 
+
+window.addEventListener('scroll', function () {
+    const navbar = document.querySelector('.navbar');
+
+    if(window.scrollY > 0) {
+        navbar.classList.add('navbar-scroll'); 
+        
+        } else {
+                navbar.classList.remove('navbar-scroll');
+            }
+        });
